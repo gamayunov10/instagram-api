768b4fb1bf78621ca12b57098fb8c249942d7614
Irek Shamilov <shamilov.irek@bk.ru>
2024-04-07 02:30:55 +0300

    add log out

--- apps/instagram-api/src/features/user/user.module.ts
+++ apps/instagram-api/src/features/user/user.module.ts
@@ -4,9 +4,10 @@ import { PrismaClient } from '@prisma/client';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UsersRepository } from './infrastructure/users.repo';
+import { UserDevicesQueryRepository } from './infrastructure/user.devices.query.repo';
 
 const services = [PrismaClient];
-const repositories = [UsersRepository];
+const repositories = [UsersRepository, UserDevicesQueryRepository];
 @Module({
   controllers: [UserController],
   providers: [UserService, ...repositories, ...services],
