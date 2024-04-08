0fc8c6fdbb894e957225cc5a71c7fc819fc74487
gamayunov10 <gamaunov1911@gmail.com>
2024-04-08 09:03:16 +0300

    docs: modified swagger docs,  example env

--- apps/instagram-api/src/features/post/post.controller.ts
+++ apps/instagram-api/src/features/post/post.controller.ts
@@ -1,43 +1,8 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 import { PostService } from './post.service';
-import { CreatePostModel } from './models/input/create-post.model';
-import { UpdatePostModel } from './models/input/update-post.model';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
-
-  @Post()
-  create(@Body() createPostDto: CreatePostModel) {
-    return this.postService.create(createPostDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.postService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostModel) {
-    return this.postService.update(+id, updatePostDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
-  }
 }
