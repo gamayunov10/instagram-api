7e566c4ab82c7d169a610022137429620f3d82a0
gamayunov10 <gamaunov1911@gmail.com>
2024-04-08 09:45:06 +0300

    fix: rm duplication

--- apps/instagram-api/src/features/user/user.module.ts
+++ apps/instagram-api/src/features/user/user.module.ts
@@ -4,11 +4,7 @@ import { PrismaClient } from '@prisma/client';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UsersRepository } from './infrastructure/users.repo';
-import { UsersQueryRepository } from './infrastructure/users.query.repo';
 import { UserDevicesQueryRepository } from './infrastructure/user.devices.query.repo';
-
-const services = [PrismaClient];
-const repositories = [UsersRepository, UsersQueryRepository];
 
 const services = [PrismaClient];
 const repositories = [UsersRepository, UserDevicesQueryRepository];
