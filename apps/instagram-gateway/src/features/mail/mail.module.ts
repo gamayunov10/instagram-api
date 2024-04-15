a8bb4109233f903e60fe78f2c77f77c98a13b91f
gamayunov10 <gamaunov1911@gmail.com>
2024-04-15 10:04:08 +0300

    perf: change to configService MailerModule

--- apps/instagram-gateway/src/features/mail/mail.module.ts
+++ apps/instagram-gateway/src/features/mail/mail.module.ts
@@ -1,38 +1,28 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import * as process from 'process';
-import { config } from 'dotenv';
-
-config();
+import { ConfigService } from '@nestjs/config';
 
 import { SendRegistrationMailUseCase } from './application/use-cases/send-registration-mail.use-case';
 import { SendPasswordRecoveryUseCase } from './application/use-cases/send-pass-recovery-mail.use-case';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        port: 465,
-        host: 'smtp.gmail.com',
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.EMAIL_PASS,
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          port: 465,
+          host: 'smtp.gmail.com',
+          auth: {
+            user: configService.get<string>('EMAIL'),
+            pass: configService.get<string>('EMAIL_PASS'),
+          },
+          secure: true,
         },
-        secure: true,
-        authMethod: 'PLAIN',
-      },
-      defaults: {
-        from: '"Sir Alex" <process.env.EMAIL>',
-      },
-      template: {
-        dir: join(__dirname, './features/mail/templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
+        defaults: {
+          from: `"Sir Alex" <${configService.get<string>('EMAIL')}>`,
         },
-      },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [SendRegistrationMailUseCase, SendPasswordRecoveryUseCase],
