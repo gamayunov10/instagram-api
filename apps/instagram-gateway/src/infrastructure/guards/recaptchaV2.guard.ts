5e44350daaf865e330fb6a892c651e3c45377e1f
Irek Shamilov <shamilov.irek@bk.ru>
2024-04-23 00:53:34 +0300

    fix: recaptchaV2.guard.ts

--- apps/instagram-gateway/src/infrastructure/guards/recaptchaV2.guard.ts
+++ apps/instagram-gateway/src/infrastructure/guards/recaptchaV2.guard.ts
@@ -4,7 +4,6 @@ import {
   ExecutionContext,
   ForbiddenException,
 } from '@nestjs/common';
-import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
 
 import { ReCaptchaResponse } from '../../base/types/re-captcha.response.type';
@@ -15,18 +14,22 @@ export class RecaptchaV2Guard implements CanActivate {
   async canActivate(context: ExecutionContext) {
     const { body } = context.switchToHttp().getRequest();
 
-    const response = await axios.post(
+    const response = await fetch(
       `https://www.google.com/recaptcha/api/siteverify`,
       {
-        response: body.reCaptcha,
-        secret: this.configService.get('GOOGLE_RECAPTCHA_SECRET_KEY'),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        method: 'POST',
+        body: `secret=${this.configService.get('RECAPTCHA_SECRET_KEY')}&response=${body.reCaptcha}`,
       },
     );
-    const data: ReCaptchaResponse = response.data;
+    const data: ReCaptchaResponse = await response.json();
 
     if (!data.success) {
       throw new ForbiddenException();
     }
+
     return true;
   }
 }
