dcfc1096679d469bca3d28184dc62fd5f368c603
gamayunov10 <gamaunov1911@gmail.com>
2024-05-15 10:56:37 +0300

    fix: email validation

--- apps/instagram-gateway/src/testing/models/input/delete-users.input.model.ts
+++ apps/instagram-gateway/src/testing/models/input/delete-users.input.model.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ArrayNotEmpty, IsArray, IsString } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsString, Matches } from 'class-validator';
+
+const emailIsIncorrect = 'Email is incorrect';
 
 export class DeleteUsersInputModel {
-  @ApiProperty({
-    type: Array,
+  @ApiProperty({ type: String, format: 'email' })
+  @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+    each: true,
+    message: emailIsIncorrect,
   })
   @IsString({ each: true })
   @ArrayNotEmpty()
