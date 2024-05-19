d42fe4c8b66dd514a72f1d80bfcb4d064c21fa78
gamayunov10 <gamaunov1911@gmail.com>
2024-05-19 09:23:46 +0300

    fix: MaxFileSizeValidator text message

--- apps/instagram-gateway/src/base/constants/constants.ts
+++ apps/instagram-gateway/src/base/constants/constants.ts
@@ -53,6 +53,8 @@ export const serverNotAvailable = 'Error! Server is not available';
 export const passwordNotSaved = 'Password not saved';
 export const invalidUserPhoto = `The photo must be less than 10 Mb and have JPEG or PNG format`;
 export const invalidImagesIds = 'images should be valid array of image ids';
+export const invalidImageInput =
+  'The photo must be less than 20 Mb and have JPEG or PNG format';
 
 // Database
 export const productionDbGuard = `You do not have permissions to perform this operation in this environment`;
