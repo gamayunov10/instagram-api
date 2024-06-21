ed70dabe9e1afa57febe1c00a9c119adb9ac508f
gamayunov10 <gamaunov1911@gmail.com>
2024-06-21 18:23:47 +0300

    build: deployment files

--- apps/instagram-gateway/src/base/application/config/file-service.config.ts
+++ apps/instagram-gateway/src/base/application/config/file-service.config.ts
@@ -7,7 +7,7 @@ export const fileServiceConfig = (): ClientsProviderAsyncOptions => {
       transport: Transport.TCP,
       options: {
         host: configService.get('FILE_SERVICE_HOST') || '0.0.0.0',
-        port: Number(configService.get('FILE_SERVICE_PORT')) || 3393,
+        port: Number(configService.get('FILE_SERVICE_PORT')) || 3438,
       },
     }),
     inject: [ConfigService],
