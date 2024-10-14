083352d2f5a0987ecd82897dc20bcdd34d208e08
Irek Shamilov <shamilov.irek@bk.ru>
2024-10-14 21:50:46 +0300

    fix: exception filter

--- apps/instagram-gateway/src/infrastructure/exception-filters/http-exception-filter.ts
+++ apps/instagram-gateway/src/infrastructure/exception-filters/http-exception-filter.ts
@@ -6,129 +6,87 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { GqlArgumentsHost, GqlContextType } from '@nestjs/graphql';
+import { GraphQLError } from 'graphql/error';
 
 import { exceptionResponseType } from '../../base/types/exception.type';
+import { ExceptionCodes } from '../../base/enums/exception-code.enum';
 
-@Catch(HttpException)
+const throwInputGraphqlError = <T>(
+  message: string,
+  status: number,
+  errorData: T,
+) => {
+  throw new GraphQLError(message, {
+    extensions: {
+      statusCode: convertToGraphQLStatus(status),
+      message: errorData,
+      code: convertToGraphQLStatus(status),
+    },
+  });
+};
+
+const convertToGraphQLStatus = (status: number): ExceptionCodes => {
+  switch (status) {
+    case HttpStatus.BAD_REQUEST:
+      return ExceptionCodes.BadRequest;
+    case HttpStatus.UNAUTHORIZED:
+      return ExceptionCodes.Unauthorized;
+    case HttpStatus.FORBIDDEN:
+      return ExceptionCodes.Forbidden;
+    case HttpStatus.NOT_FOUND:
+      return ExceptionCodes.NotFound;
+    default:
+      return ExceptionCodes.InternalServerError;
+  }
+};
+
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost): void {
+  catch(exception: HttpException, host: ArgumentsHost) {
     const hostType = host.getType<GqlContextType>();
+    if (hostType === 'graphql') {
+      if (exception instanceof HttpException) {
+        const gqlHost = GqlArgumentsHost.create(host); // eslint-disable-line
 
-    switch (hostType) {
-      case 'graphql':
-        this.handleGraphQL(exception, host);
-        break;
-      case 'http':
-        this.handleHttp(exception, host);
-        break;
-      case 'ws':
-        this.handleWebSocket(exception, host);
-        break;
-      default:
-        throw new Error('Unknown context type');
+        const response = exception.getResponse();
+        const status = exception.getStatus();
+
+        const messageResponse = exception.message;
+        throwInputGraphqlError(messageResponse, status, response);
+      }
+      return exception;
     }
-  }
 
-  private handleGraphQL(exception: HttpException, host: ArgumentsHost): void {
-    const gqlHost = GqlArgumentsHost.create(host);
-    const graphqlResponse = gqlHost.getContext(); // Получаем context для GraphQL
-
-    const status = exception.getStatus();
-    const responseBody: any = exception.getResponse();
-
-    switch (status) {
-      case HttpStatus.BAD_REQUEST:
-      case HttpStatus.NOT_FOUND:
-      case HttpStatus.UNAUTHORIZED:
-      case HttpStatus.INTERNAL_SERVER_ERROR:
-      case HttpStatus.FORBIDDEN:
-        if (typeof responseBody.message !== 'string') {
-          graphqlResponse.errors = responseBody.message;
-        } else {
-          graphqlResponse.errors = [{ message: responseBody.message }];
-        }
-        break;
-
-      default:
-        graphqlResponse.errors = [
-          {
-            statusCode: status,
-            message: 'Unknown error occurred',
-            timestamp: new Date().toISOString(),
-          },
-        ];
-        break;
-    }
-  }
-
-  private handleHttp(exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
     const status = exception.getStatus();
 
-    switch (status) {
-      case HttpStatus.BAD_REQUEST:
-      case HttpStatus.NOT_FOUND:
-      case HttpStatus.UNAUTHORIZED:
-      case HttpStatus.INTERNAL_SERVER_ERROR:
-      case HttpStatus.FORBIDDEN:
-        const errorsResponse: exceptionResponseType = {
-          errorsMessages: [],
-        };
-        const responseBody: any = exception.getResponse();
+    if (
+      status === HttpStatus.BAD_REQUEST ||
+      status === HttpStatus.NOT_FOUND ||
+      status === HttpStatus.UNAUTHORIZED ||
+      status === HttpStatus.INTERNAL_SERVER_ERROR
+    ) {
+      const errorsResponse: exceptionResponseType = {
+        errorsMessages: [],
+      };
+      const responseBody: any = exception.getResponse();
 
-        if (typeof responseBody.message !== 'string') {
-          responseBody.message.forEach((m) =>
-            errorsResponse.errorsMessages.push(m),
-          );
-          response.status(status).json(errorsResponse);
-        } else {
-          response.status(status).json({
-            message: responseBody.message,
-            statusCode: status,
-            timestamp: new Date().toISOString(),
-          });
-        }
-        break;
-
-      default:
-        response.status(status).json({
-          statusCode: status,
-          timestamp: new Date().toISOString(),
-          path: request.url,
-        });
-        break;
-    }
-  }
-
-  private handleWebSocket(exception: HttpException, host: ArgumentsHost): void {
-    const wsHost = host.switchToWs();
-    const wsResponse = wsHost.getClient();
-
-    const status = exception.getStatus();
-    const responseBody: any = exception.getResponse();
-
-    switch (status) {
-      case HttpStatus.BAD_REQUEST:
-      case HttpStatus.NOT_FOUND:
-      case HttpStatus.UNAUTHORIZED:
-      case HttpStatus.INTERNAL_SERVER_ERROR:
-      case HttpStatus.FORBIDDEN:
-        if (typeof responseBody.message !== 'string') {
-          wsResponse.emit('error', responseBody.message);
-        } else {
-          wsResponse.emit('error', { message: responseBody.message });
-        }
-        break;
-
-      default:
-        wsResponse.emit('error', {
-          statusCode: status,
-          message: 'Unknown error occurred',
-          timestamp: new Date().toISOString(),
-        });
-        break;
+      if (typeof responseBody.message !== 'string') {
+        responseBody.message.forEach((m) =>
+          errorsResponse.errorsMessages.push(m),
+        );
+        response.status(status).json(errorsResponse);
+      } else {
+        response.status(status).json(responseBody.message);
+      }
+    } else {
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
     }
   }
 }
