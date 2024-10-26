56071a3789e706650fa915351e7ee5c45602d65c
Irek Shamilov <shamilov.irek@bk.ru>
2024-10-26 19:04:35 +0300

    feat: add auth login grql and fix socket

--- apps/instagram-gateway/src/resolvers/auth/auth.resolver.ts
+++ apps/instagram-gateway/src/resolvers/auth/auth.resolver.ts
@@ -1,15 +1,30 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { BasicGqlGuard } from '../../infrastructure/guards/basic-gql-guard.service';
+import { AuthBasicGqlGuard } from '../../infrastructure/guards/auth-gql-guard';
+import { AuthService } from '../../features/auth/api/application/auth.service';
+
+import { AuthLoginInput } from './models/auth-login-input.model';
 
 @Resolver()
 export class AuthResolver {
-  constructor() {}
+  constructor(private readonly authService: AuthService) {}
 
   @Query(() => String)
   @UseGuards(BasicGqlGuard)
   loginSa(): string {
     return 'Authorized user';
+  }
+
+  @Mutation(() => String)
+  @UseGuards(AuthBasicGqlGuard)
+  async authorizeSuperAdmin(
+    @Args('authLoginInput') authLoginInput: AuthLoginInput,
+  ): Promise<string> {
+    return this.authService.createBasicAuthString(
+      authLoginInput.email,
+      authLoginInput.password,
+    );
   }
 }
