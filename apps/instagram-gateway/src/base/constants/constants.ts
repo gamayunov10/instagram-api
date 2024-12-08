bffe12e88cc2b194d78656230c8bc6376daf4ea7
Irek Shamilov <shamilov.irek@bk.ru>
2024-12-08 10:57:58 +0300

    fix: ban reason

--- apps/instagram-gateway/src/base/constants/constants.ts
+++ apps/instagram-gateway/src/base/constants/constants.ts
@@ -35,6 +35,8 @@ export const usernameIsIncorrect = 'The username must contain a-zA-Z0-9_-';
 export const firstNameIsIncorrect =
   'The firstName must contain A-Z a-z А-Я а-я';
 export const lastNameIsIncorrect = 'The lastName must contain A-Z a-z А-Я а-я';
+export const banReasonIsIncorrect =
+  'The firstName must contain A-Z a-z А-Я а-я';
 export const minChar1 = 'Minimum number of characters 1';
 export const minChar6 = 'Minimum number of characters 6';
 export const maxChar20 = 'Maximum number of characters 20';
